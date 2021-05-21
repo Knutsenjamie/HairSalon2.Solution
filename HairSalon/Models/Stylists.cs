a3965243dab9131c01701f21ae8c0c8e1a17a014
“Jamie <knutsenjamie@yahoo.com>
2021-05-21 11:58:50 -0700

    add constructor/class to stylists.cs

---
+++ HairSalon/Models/Stylists.cs
@@ -0,0 +1,9 @@
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Details { get; set; }
+    public int ClientId { get; set; }
+    public virtual Client Client { get; set; }
